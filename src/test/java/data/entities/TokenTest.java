b62d8bc204c424be520904d8e9d87fecc697b097
Sandra <sandrachacon@alumnos.upm.es>
2016-03-03 16:35:32 +0100

    Añadido test en data/entities/TokenTest para isValid y pasados

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -17,5 +17,17 @@ public class TokenTest {
         Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
     }
+    
+    @Test
+    public void testTokenIsValid()
+    {
+    	User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        Token token = new Token(user);
+        assertTrue(token.isValid());
+        Calendar date = Calendar.getInstance();
+        date.set(2016, Calendar.MARCH, 01);
+        token.setCreatedDate(date);
+        assertTrue(!token.isValid());
+    }
 
 }
