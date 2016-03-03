0f459b0e3a3bf574409d98ae39126f535f8fa3df
Sandra <sandrachacon@alumnos.upm.es>
2016-03-03 17:47:05 +0100

    Refactor TokenTest

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -4,25 +4,30 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import data.entities.Token;
 import data.entities.User;
 
 public class TokenTest {
+	
+	private User user;
+	private Token token;
+	
+	@Before
+	public void before(){
+		this.user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+		this.token = new Token(user);
+	}
 
     @Test
     public void testTokenUser() {
-        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
     }
     
     @Test
-    public void testTokenIsValid()
-    {
-    	User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
+    public void testTokenIsValid(){
         assertTrue(token.isValid());
         Calendar date = Calendar.getInstance();
         date.set(2016, Calendar.MARCH, 01);
