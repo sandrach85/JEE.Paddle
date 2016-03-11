457f079be47d35d24fb4565382eb9641e66820f1
Sandra <sandrachacon@alumnos.upm.es>
2016-03-11 18:06:35 +0100

    Refactor TrainingDaoITest

--- src/test/java/data/daos/TrainingDaoITest.java
+++ src/test/java/data/daos/TrainingDaoITest.java
@@ -1,5 +1,7 @@
 package data.daos;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import config.PersistenceConfig;
 import config.TestsPersistenceConfig;
+import data.entities.Training;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class})
@@ -16,9 +19,14 @@ public class TrainingDaoITest {
 	@Autowired
 	private TrainingDao trainingDao;
 	
+    @Autowired
+    private DaosService daosService;
+	
 	@Test
-	public void testFindById(int id){
-		//Training training = (Training) daoService.getMap().get("");
+	public void testFindById(){
+		Training training = (Training) daosService.getMap().get("uT5");
+		System.out.println("------TRAINING------"+training);
+		assertEquals(training , trainingDao.findById(training.getId()));
 		
 	}
 
