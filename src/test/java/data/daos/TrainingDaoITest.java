5d14a51b3bd8045a62601d1a85767c6d84572a68
Sandra <sandrachacon@alumnos.upm.es>
2016-03-11 19:15:59 +0100

    Añadidos test en TrainingDaoITest

--- src/test/java/data/daos/TrainingDaoITest.java
+++ src/test/java/data/daos/TrainingDaoITest.java
@@ -2,6 +2,8 @@ package data.daos;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import config.PersistenceConfig;
 import config.TestsPersistenceConfig;
 import data.entities.Training;
+import data.entities.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class})
@@ -21,13 +24,45 @@ public class TrainingDaoITest {
 	
     @Autowired
     private DaosService daosService;
+    
+    @Autowired
+    private UserDao userDao;
 	
 	@Test
 	public void testFindById(){
-		Training training = (Training) daosService.getMap().get("uT5");
+		Training training = (Training) daosService.getMap().get("uT41");
 		System.out.println("------TRAINING------"+training);
 		assertEquals(training , trainingDao.findById(training.getId()));
-		
 	}
 
+	@Test
+	public void testDeleteTrainingAndReserves(){
+		Training training = (Training) daosService.getMap().get("uT41");
+		int id = training.getId();
+		trainingDao.deleteTrainingAndReserves(training.getId());
+		assertNull(trainingDao.findById(id));
+	}
+	
+	@Test
+	public void testDeleteUserTraining(){
+		Training training = (Training) daosService.getMap().get("uT41");
+		List<User> usersBef = trainingDao.findUsersTraining(training.getId());
+		trainingDao.deleteUserTraining(training.getId(), 2);
+		List<User> usersAft = trainingDao.findUsersTraining(training.getId());
+		assertTrue(usersBef!=usersAft);
+	}
+	
+	@Test
+	public void testFindIdTrainer(){
+		Training training = (Training) daosService.getMap().get("uT41");
+		assertTrue(training.getTrainer().getId()==6);
+	}
+	
+	@Test
+	public void testCreateTraining(){
+		Training training = (Training) daosService.getMap().get("uT41");
+		Training trainingC = trainingDao.createTraining(training.getDateIni(), training.getDateEnd(), training.getCourt(), training.getTrainer(), 12);
+		assertEquals(4,trainingC.getId());
+	}
+	
 }
