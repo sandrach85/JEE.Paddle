0089a3f631fb6857cd55d4d8002999f62f854f55
Sandra <Sandra@192.168.0.164>
2016-03-13 21:33:40 +0100

    Modificada cte max_alumnos en entidad Training

--- src/main/java/data/entities/Training.java
+++ src/main/java/data/entities/Training.java
@@ -17,146 +17,144 @@ import javax.persistence.OneToOne;
 @Entity
 public class Training {
 
-	@Id
-	@GeneratedValue
-	private int id;
+    @Id
+    @GeneratedValue
+    private int id;
 
-	private Calendar dateIni;
+    private Calendar dateIni;
 
-	private Calendar dateEnd;
+    private Calendar dateEnd;
 
-	@ManyToOne
-	@JoinColumn
-	private Court court;
+    @ManyToOne
+    @JoinColumn
+    private Court court;
 
-	@OneToOne
-	@JoinColumn
-	private User trainer;
+    @OneToOne
+    @JoinColumn
+    private User trainer;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	private List<User> users;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<User> users;
 
-	public static final int MAX_USERS = 4;
+    public final int MAX_USERS = 4;
 
-	public Training() {
-		this.users = new ArrayList<User>();
-	}
+    public Training() {
+        this.users = new ArrayList<User>();
+    }
 
-	public Training(Calendar dateIni, Calendar dateEnd, Court court, User trainer) {
-		super();
-		this.dateIni = dateIni;
-		this.dateEnd = dateEnd;
-		this.court = court;
-		this.trainer = trainer;
-		this.users = new ArrayList<User>();
-	}
+    public Training(Calendar dateIni, Calendar dateEnd, Court court, User trainer) {
+        super();
+        this.dateIni = dateIni;
+        this.dateEnd = dateEnd;
+        this.court = court;
+        this.trainer = trainer;
+        this.users = new ArrayList<User>();
+    }
 
-	public Calendar getDateIni() {
-		return dateIni;
-	}
+    public Calendar getDateIni() {
+        return dateIni;
+    }
 
-	public void setDateIni(Calendar dateIni) {
-		this.dateIni = dateIni;
-	}
+    public void setDateIni(Calendar dateIni) {
+        this.dateIni = dateIni;
+    }
 
-	public Calendar getDateEnd() {
-		return dateEnd;
-	}
+    public Calendar getDateEnd() {
+        return dateEnd;
+    }
 
-	public void setDateEnd(Calendar dateEnd) {
-		this.dateEnd = dateEnd;
-	}
+    public void setDateEnd(Calendar dateEnd) {
+        this.dateEnd = dateEnd;
+    }
 
-	public Court getCourt() {
-		return court;
-	}
+    public Court getCourt() {
+        return court;
+    }
 
-	public void setCourt(Court court) {
-		this.court = court;
-	}
+    public void setCourt(Court court) {
+        this.court = court;
+    }
 
-	public List<User> getUsers() {
-		return users;
-	}
+    public List<User> getUsers() {
+        return users;
+    }
 
-	public void setUsers(List<User> users) {
-		this.users = users;
-	}
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public User getTrainer() {
-		return trainer;
-	}
+    public User getTrainer() {
+        return trainer;
+    }
 
-	public void setTrainer(User trainer) {
-		this.trainer = trainer;
-	}
+    public void setTrainer(User trainer) {
+        this.trainer = trainer;
+    }
 
-	@Override
-	public int hashCode() {
-		return id;
-	}
+    @Override
+    public int hashCode() {
+        return id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Training other = (Training) obj;
-		if (court == null) {
-			if (other.court != null)
-				return false;
-		} else if (!court.equals(other.court))
-			return false;
-		if (dateEnd == null) {
-			if (other.dateEnd != null)
-				return false;
-		} else if (!dateEnd.equals(other.dateEnd))
-			return false;
-		if (dateIni == null) {
-			if (other.dateIni != null)
-				return false;
-		} else if (!dateIni.equals(other.dateIni))
-			return false;
-		if (id != other.id)
-			return false;
-		if (trainer == null) {
-			if (other.trainer != null)
-				return false;
-		} else if (!trainer.equals(other.trainer))
-			return false;
-		if (users == null) {
-			if (other.users != null)
-				return false;
-		} else if (!users.equals(other.users))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Training other = (Training) obj;
+        if (court == null) {
+            if (other.court != null)
+                return false;
+        } else if (!court.equals(other.court))
+            return false;
+        if (dateEnd == null) {
+            if (other.dateEnd != null)
+                return false;
+        } else if (!dateEnd.equals(other.dateEnd))
+            return false;
+        if (dateIni == null) {
+            if (other.dateIni != null)
+                return false;
+        } else if (!dateIni.equals(other.dateIni))
+            return false;
+        if (id != other.id)
+            return false;
+        if (trainer == null) {
+            if (other.trainer != null)
+                return false;
+        } else if (!trainer.equals(other.trainer))
+            return false;
+        if (users == null) {
+            if (other.users != null)
+                return false;
+        } else if (!users.equals(other.users))
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		String dateI = new SimpleDateFormat("dd-MMM-yyyy HH:mm").format(dateIni.getTime());
-		String dateE = new SimpleDateFormat("dd-MMM-yyyy HH:mm").format(dateEnd.getTime());
-		return "Training [id=" + id + ", dateIni=" + dateI + ", dateEnd=" + dateE + ", court=" + court + ", trainer="
-				+ trainer + ", users=" + users + "]";
-	}
+    @Override
+    public String toString() {
+        String dateI = new SimpleDateFormat("dd-MMM-yyyy HH:mm").format(dateIni.getTime());
+        String dateE = new SimpleDateFormat("dd-MMM-yyyy HH:mm").format(dateEnd.getTime());
+        return "Training [id=" + id + ", dateIni=" + dateI + ", dateEnd=" + dateE + ", court=" + court + ", trainer=" + trainer + ", users="
+                + users + "]";
+    }
 
+    public void addUserInTraining(User user) {
+        if (this.users.size() < MAX_USERS) {
+            this.users.add(user);
+        }
+    }
 
-
-	public void addUserInTraining(User user) {
-		if (this.users.size() < Training.MAX_USERS) {
-			this.users.add(user);
-		}
-	}
-
-	public void deleteUser(int idU) {
-		users.remove(users.get(idU));
-	}
+    public void deleteUser(int idU) {
+        users.remove(users.get(idU));
+    }
 
 }
