df2faed7d2eea80651475c6ac659d1c19b238210
Sandra <sandrachacon@alumnos.upm.es>
2016-03-14 09:32:12 +0100

    Quitado interceptor

--- src/main/java/business/controllers/TokenValidateAccessInterceptor.java
+++
@@ -1,7 +0,0 @@
-package business.controllers;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class TokenValidateAccessInterceptor  extends HandlerInterceptorAdapter{
-
-}
--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -4,28 +4,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import business.api.Uris;
-import business.controllers.TokenValidateAccessInterceptor;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS, ResourceNames.SERVICES})
+@ComponentScan(basePackages = { ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS,
+		ResourceNames.SERVICES })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    // CORS
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").maxAge(3600);
-    }
-    
-    // Se configuran los interceptores
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenValidateAccessInterceptor()).addPathPatterns(Uris.SERVLET_MAP + "/**")
-                .excludePathPatterns("/foo/**");
-    }
+	// CORS
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").maxAge(3600);
+	}
 
 }
