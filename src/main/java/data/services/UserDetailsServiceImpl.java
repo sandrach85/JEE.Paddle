1c54a57dad9e63d0b2a917f86ce442ab4c047e12
Sandra <sandrachacon@alumnos.upm.es>
2016-03-14 09:51:08 +0100

    ActualizadoUserDetailServiceImpl

--- src/main/java/data/services/UserDetailsServiceImpl.java
+++ src/main/java/data/services/UserDetailsServiceImpl.java
@@ -24,38 +24,39 @@ import data.entities.User;
 @Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private UserDao userDao;
+	@Autowired
+	private UserDao userDao;
 
-    @Autowired
-    private AuthorizationDao authorizationDao;
+	@Autowired
+	private AuthorizationDao authorizationDao;
 
-    @Override
-    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        User user = userDao.findByTokenValue(username);
-        if (user == null) {
-            user = userDao.findByUsernameOrEmail(username);
-            if (user == null) {
-                throw new UsernameNotFoundException("Usuario no encontrado");
-            } else {
-                return this.userBuilder(user.getUsername(), user.getPassword(), Arrays.asList(Role.AUTHENTICATED));
-            }
-        } else {
-            List<Role> roleList = authorizationDao.findRoleByUser(user);
-            return this.userBuilder(user.getUsername(), new BCryptPasswordEncoder().encode(""), roleList);
-        }
-    }
+	@Override
+	public UserDetails loadUserByUsername(final String usernameOrEmailOrTokenValue) throws UsernameNotFoundException {
+		User user = userDao.findByTokenValue(usernameOrEmailOrTokenValue);
+		if (user != null) {
+			List<Role> roleList = authorizationDao.findRoleByUser(user);
+			return this.userBuilder(user.getUsername(), new BCryptPasswordEncoder().encode(""), roleList);
+		} else {
+			user = userDao.findByUsernameOrEmail(usernameOrEmailOrTokenValue);
+			if (user != null) {
+				return this.userBuilder(user.getUsername(), user.getPassword(), Arrays.asList(Role.AUTHENTICATED));
+			} else {
+				throw new UsernameNotFoundException("Usuario no encontrado");
+			}
+		}
+	}
 
-    private org.springframework.security.core.userdetails.User userBuilder(String username, String password, List<Role> roles) {
-        boolean enabled = true;
-        boolean accountNonExpired = true;
-        boolean credentialsNonExpired = true;
-        boolean accountNonLocked = true;
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        for (Role role : roles) {
-            authorities.add(new SimpleGrantedAuthority(role.roleName()));
-        }
-        return new org.springframework.security.core.userdetails.User(username, password, enabled, accountNonExpired,
-                credentialsNonExpired, accountNonLocked, authorities);
-    }
+	private org.springframework.security.core.userdetails.User userBuilder(String username, String password,
+			List<Role> roles) {
+		boolean enabled = true;
+		boolean accountNonExpired = true;
+		boolean credentialsNonExpired = true;
+		boolean accountNonLocked = true;
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for (Role role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role.roleName()));
+		}
+		return new org.springframework.security.core.userdetails.User(username, password, enabled, accountNonExpired,
+				credentialsNonExpired, accountNonLocked, authorities);
+	}
 }
