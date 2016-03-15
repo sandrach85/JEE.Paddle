92c2dd87263445bfbb7f8c29ad390159aaa29ed7
Sandra <sandrachacon@alumnos.upm.es>
2016-03-15 11:33:40 +0100

    Creado test prueba autorizacion en createTraining

--- src/test/java/api/TrainingResourceFuncionalITesting.java
+++ src/test/java/api/TrainingResourceFuncionalITesting.java
@@ -1,28 +1,52 @@
 package api;
 
-import java.util.Calendar;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
+import business.api.Uris;
+import config.PersistenceConfig;
+import config.TestsPersistenceConfig;
 import data.daos.DaosService;
+import data.entities.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { PersistenceConfig.class, TestsPersistenceConfig.class })
 public class TrainingResourceFuncionalITesting {
-    
-    @Autowired
-    private DaosService daosService;
-    
-    @Test
-    public void testCreateTraining(){
-      /*  Calendar date1 = Calendar.getInstance();
-        Calendar date2 = Calendar.getInstance();
-        date2.set(date1.get(Calendar.YEAR), date1.get(Calendar.MONTH)+2, date1.get(Calendar.DATE), date1.get(Calendar.HOUR_OF_DAY), date1.get(Calendar.MINUTE));
-        CourtState court = new CourtState(1, true);
-        Training training = (Training) daosService.getMap().get("u62");
-        UserWrapper user = (UserWrapper) daosService.getMap().get("u2");
-        TrainingWrapper trainingWrapper = new TrainingWrapper(date1,date2,court,user);
-        assertTrue();*/
-        
-    }
 
+	@Autowired
+	private DaosService daosService;
+
+	@Test
+	public void testErrorUnauthorizedCreateTraining() {
+		try {
+			User u1 = (User) daosService.getMap().get("u1");
+			String userName = u1.getUsername();
+			String pws = u1.getPassword();
+			new RestBuilder<Object>(Uris.SERVLET_MAP).path(Uris.TRAINING).path(Uris.CREATE_TRAINING)
+					.basicAuth(userName, pws).get().build();
+			fail();
+		} catch (HttpClientErrorException httpError) {
+			assertEquals(HttpStatus.UNAUTHORIZED, httpError.getStatusCode());
+			System.out.println("ERROR>>>>> " + httpError.getMessage());
+		}
+	}
+
+	@Test
+	public void testAuthorizedOKCreateTraining() {
+		User u1 = (User) daosService.getMap().get("u6");
+		String userName = u1.getUsername();
+		String pws = u1.getPassword();
+		Object response = new RestBuilder<Object>(Uris.SERVLET_MAP).path(Uris.TRAINING).path(Uris.CREATE_TRAINING)
+				.basicAuth(userName, pws).clazz(Object.class).get().build();
+
+		System.out.println("INFO >>>>> " + u1.toString());
+		System.out.println("INFO >>>>> " + response);
+	}
 }
