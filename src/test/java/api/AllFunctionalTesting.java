ea59407f835cded9dfb30a99124d07155689e583
Sandra <sandrachacon@alumnos.upm.es>
2016-03-17 14:38:38 +0100

    Añadidos test de ReserveResourceFunctionalTesting

--- src/test/java/api/AllFunctionalTesting.java
+++ src/test/java/api/AllFunctionalTesting.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite.SuiteClasses;
     UserResourceFunctionalTesting.class, 
     TokenResourceFunctionalTesting.class, 
     CourtResourceFunctionalTesting.class,
-    TrainingResourceFunctionalTesting.class,
+    ReserveResourceFunctionalTesting.class,
+    TrainingResourceFunctionalTesting.class
 })
 public class AllFunctionalTesting {
 
